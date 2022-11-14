e4ce75f80d658bcc3746d0c2f30446bd9c38422e
Sebastian Post <115038252+ZynCitrus@users.noreply.github.com>
2022-11-14 14:16:25 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read
+
+firefox -new-tab https://elevera.itslearning.com/
+
+firefox -new-tab https://web.snapchat.com/
+
+firefox -new-tab https://postmodernistx.github.io/youthful-dinosaurus/
+
+firefox -new-tab https://stackoverflow.com/
+
+firefox -new-tab https://reddit.com/
+
+firefox -new-tab https://github.com/Medieinstitutet/fed22d-js-grundkurs-gruppuppgift-webbshop-skull-commando-squad
+
+firefox -new-tab https://soundcloud.com
+
+firefox -new-tab https://www.w3schools.com/
